a9ad31f39d529e584294a296034fc017de4d2d7b
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2025-02-02 00:59:58 +0530

    Scheduler v2 - Time Sequencing

--- asynco.py
+++ asynco.py
@@ -1,6 +1,6 @@
 import time
 from collections import deque
-
+import heapq
 
 class Scheduler: 
     def __init__(self):
@@ -12,14 +12,13 @@ class Scheduler:
 
     def call_later(self, delay, func):
         deadline = time.time() + delay
-        self.sleeping.append((deadline,func))
-        self.sleeping.sort()
+        heapq.heappush(self.sleeping, (deadline, func))
 
     def run(self):
         while self.ready or self.sleeping: 
             if not self.ready: 
                 # Find the nearest deadline 
-                deadline, func = self.sleeping.pop(0)
+                deadline, func = heapq.heappop(self.sleeping)
                 delta = deadline - time.time()
                 if delta > 0:
                     time.sleep(delta)
@@ -34,16 +33,19 @@ sched = Scheduler()
 def countdown(n):
     if n > 0:
         print('Down', n)
+        # time.slee(4)          # Blocking call (nothing else can run)
         sched.call_later(4, lambda: countdown(n-1))
 
 
 def countup(stop, x=0):
     if x < stop:
         print('Up', x)
+        # time.slee(1)          # Blocking call (nothing else can run)
         sched.call_later(1, lambda: countup(stop, x+1))
-
-# Problem: How to achieve concurrency without threads? 
-# Issue: Figure out how to switch between tasks.
+        
 sched.call_soon(lambda: countdown(5))
 sched.call_soon(lambda: countup(20))
 sched.run()
+
+# Problem: How to achieve concurrency without threads? 
+# Issue: Figure out how to switch between tasks.
