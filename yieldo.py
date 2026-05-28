317a59eb7a87dbc6c1d3ac426e7623d654891936
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2026-05-28 11:41:47 +0530

    in-the-middle-of-it yeildo.py

---
+++ yieldo.py
@@ -0,0 +1,42 @@
+import time
+from collections import deque
+
+class Scheduler: 
+    def __init__(self):
+        self.ready = deque()
+
+    def new_task(self, gen):
+        self.ready.append(gen)
+
+    def run(self):
+        while self.ready:
+            self.current = self.ready.popleft()
+            # Drive as a generator 
+            try: 
+                next(self.current)
+                if self.current:
+                    self.ready.append(self.current)
+            except StopIteration:
+                pass
+
+sched = Scheduler()         # Background scheduler object
+ 
+def countdown(n):
+    while n > 0:
+        print('Down', n)
+        time.sleep(1)
+        yield
+        n -= 1
+
+def countup(stop):
+    x = 0
+    while x<stop:
+        print('Up', x)
+        time.sleep(1)
+        yield
+        x += 1
+
+
+sched.new_task(countdown(5))
+sched.new_task(countup(5))
+sched.run()
